e4b7f60a45567311185ebd4ca84d3292ad39c3e5
Tanmay <30860087+tanmayagrawal39@users.noreply.github.com>
2018-05-28 01:23:03 +0530

    Add files via upload

---
+++ Scraping/Crawler/main.py
@@ -0,0 +1,52 @@
+import threading
+from queue import Queue
+from spider import Spider
+from domain import *
+from general import *
+import time
+
+PROJECT_NAME = 'moneycontrol'
+HOMEPAGE = 'https://www.moneycontrol.com/'
+DOMAIN_NAME = get_domain_name(HOMEPAGE)
+QUEUE_FILE = PROJECT_NAME + '/queue.txt'
+CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
+NUMBER_OF_THREADS = 8
+queue = Queue()
+Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
+
+# Create worker threads (will die when main exits)
+def create_workers():
+    for _ in range(NUMBER_OF_THREADS):
+        t = threading.Thread(target=work)
+        t.daemon = True
+        t.start()
+
+
+# Do the next job in the queue
+def work():
+    while True:
+        url = queue.get()
+        Spider.crawl_page(threading.current_thread().name, url)
+        queue.task_done()
+
+
+# Each queued link is a new job
+def create_jobs():
+    for link in file_to_set(QUEUE_FILE):
+        queue.put(link)
+    queue.join()
+    time.sleep(1)
+    crawl()
+
+
+# Check if there are items in the queue, if so crawl them
+def crawl():
+    queued_links = file_to_set(QUEUE_FILE)
+    if len(queued_links) > 0:
+        print(str(len(queued_links)) + ' links in the queue')
+        create_jobs()
+
+
+create_workers()
+crawl()
+
